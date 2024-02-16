c2c08f2ca3901698e335f63170ef814e09d67927
javimacias@gmail.com <javimacias@gmail.com>
2024-02-16 09:36:49 +0100

    last changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
 
 
-const password = process.argv[2]
+
 
 const url =
-  `mongodb+srv://javipilgrim:${password}@cluster0.ndstpvu.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://javipilgrim:mongopilgrim@cluster0.ndstpvu.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 
 const requestLogger = (request, response, next) => {
@@ -66,36 +67,27 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    console.log("note save!!")
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+  })
 })
+
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -106,7 +98,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
