37c262ae0299f53556e15d831b25301bfda392f4
javimacias@gmail.com <javimacias@gmail.com>
2024-02-20 12:55:10 +0100

    validations

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const Note = require('./models/note')
 
 
 
-const url =
-  `mongodb+srv://javipilgrim:mongopilgrim@cluster0.ndstpvu.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URI
 
 
 const requestLogger = (request, response, next) => {
@@ -24,8 +23,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   } 
-
   next(error)
 }
 
@@ -49,7 +49,7 @@ app.get('/api/notes', (request, response) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -65,6 +65,7 @@ app.post('/api/notes', (request, response) => {
     console.log("note save!!")
     response.json(savedNote)
   })
+  .catch(error=>next(error))
 })
 
 app.get('/api/notes/:id', (request, response,next) => {
@@ -90,14 +91,13 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
