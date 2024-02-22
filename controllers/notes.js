2dd99fd90e3e270840b28401fb33812a399f70b6
javimacias@gmail.com <javimacias@gmail.com>
2024-02-22 10:28:37 +0100

    ordering in modules

---
+++ controllers/notes.js
@@ -0,0 +1,67 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+})
+
+
+notesRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    note.save().then(savedNote => {
+        console.log('note save!!')
+        response.json(savedNote)
+    })
+        .catch(error=>next(error))
+})
+
+notesRouter.get('/:id', (request, response,next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+
+notesRouter.delete('/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            console.log(result)
+            response.status(204).end()
+            console.log('person deleted!')
+        })
+        .catch(error => next(error))
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+    const { content, important } = request.body
+
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        { content, important },
+        { new: true, runValidators: true, context: 'query' }
+    ) 
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+module.exports = notesRouter
